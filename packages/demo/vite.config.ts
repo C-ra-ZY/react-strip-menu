5956967a4dbb43d09f8dcc3a6552987aa8b70e95
Carl.Chen <ayang879@gmail.com>
2026-02-27 20:01:54 +0800

    fix: update production base path in Vite config for demo

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/react-strip-menu-demo/" : "/",
+  base: mode === "production" ? "/react-strip-menu/" : "/",
   server: {
     port: 3456,
     open: true,
